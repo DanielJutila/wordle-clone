522f658e21a1ab43100cfa6f1d5941bfee60faa6
Daniel Jutila <daniel.jutila@gmail.com>
2024-01-11 16:21:02 -0600

    Working words showing up on screen with correct API

--- assets/scripts/keyboardEvents.js
+++ assets/scripts/keyboardEvents.js
@@ -1,17 +1,27 @@
-let word = $('.letter-box');
 let attempt = 0;
 let guessedLetter = [];
 
+//gets the word of the day
+getWotd();
+let wotd = []
+function getWotd() {
+  getApi().then(function (result) {
+    wotd = result.toUpperCase().split('');
+  })
+}
+
+//Checks to see if you pressed enter
 $(document).on("keydown", function (event) {
   if (event.which === 13) {
-    let row = $('.row-' + attempt);
     //checks to make sure length is 5
-    if(guessedLetter.length === 5){
+    if (guessedLetter.length === 5) {
+      enterElement();
       //clears array if both requirments pass
-    while(guessedLetter.length > 0){
-      guessedLetter.pop();
+      while (guessedLetter.length > 0) {
+        guessedLetter.pop();
+      }
+      attempt++;
     }
-  }
   }
 });
 
@@ -19,22 +29,45 @@ $(document).on("keydown", function (event) {
 $(document).on('keydown', function (event) {
   event.preventDefault();
   let key = String.fromCharCode(event.which);
-
   //removes letter from array
-  if(event.which === 8){
+  if (event.which === 8) {
     guessedLetter.pop();
-    console.log(guessedLetter);
-
-    return;
   }
   //checks to make sure it is letter being pressed
   //adds letter to array
   if (!/[^a-zA-Z]/.test(key)) {
-    if(guessedLetter.length < 5){
-    guessedLetter.push(key);
-    console.log(guessedLetter);
-
+    if (guessedLetter.length < 5) {
+      guessedLetter.push(key);
+      console.log(wotd);
+      console.log(guessedLetter);
     }
   }
+  showLetter();
 });
+
+//shows letters on screen
+function showLetter() {
+  $('.row-' + [attempt] + ' > div').each(function (index, element) {
+    if (index < guessedLetter.length) {
+      $(element).text(guessedLetter[index]);
+    } else {
+      $(element).text('');
+    }
+  });
+}
+function enterElement() {
+  let i = 0;
+  let guessed = [];
+  $('.row-' + [attempt] + ' > div').each(function (index, element) {
+    guessed.push($(element).text())
+  });
+  for(let g of guessed){
+    if(g === wotd[i]){
+      console.log('correct');
+    } else {
+      console.log('wrong')
+    }
+    i++;
+  }
+}
 
--- assets/scripts/randomWordAPI.js
+++ assets/scripts/randomWordAPI.js
@@ -1,12 +1,15 @@
-function getApi(){
-    var requestUrl = 'https://random-word-api.vercel.app/api?words=1&length=5';
-    
-  fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    return data;
-});
+function getApi() {
+  var requestUrl = 'https://random-word-api.vercel.app/api?words=1&length=5';
 
+  return fetch(requestUrl)
+    .then(function (response) {
+      // Check if the request was successful (status code 200)
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      return data[0];
+    });
 }
